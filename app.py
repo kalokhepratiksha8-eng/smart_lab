6d8e9c3e7b153e1dbf516b22a4756bb6bc584803
kalokhepratiksha8-eng <kalokhepratiksha8@gmail.com>
2026-07-06 14:48:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def get_db():
     return mysql.connector.connect(
         host="brwvcod6w5wihegpcl5u-mysql.services.clever-cloud.com",
         user="up7j4qriyyjqvsfe",
-        password="P@K!2816#P",
+        password="gjkQCWk577CCY7GMpE0G",
         database="brwvcod6w5wihegpcl5u",
         port=3306
     )
